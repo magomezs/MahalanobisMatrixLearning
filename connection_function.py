2dbbb0e4fd0ddd2db94adaadd1c708dff78913e1
magomezs <mariajgomezsilva@gmail.com>
2018-04-20 10:53:58 +0100

    Update connection_function.py

--- connection_function.py
+++ connection_function.py
@@ -7,12 +7,15 @@ import sys
 class ConnectionFunction(caffe.Layer):
 	def setup(self, bottom, top):
 		if len(bottom)!=3:
-			raise Exception('must have exactly three inputs: 2 descriptors and 1 MM')
+			raise Exception('must have exactly three inputs: 2 features arrays and 1 MM (Mahalanobis Matrix)')
 		if len(top)!=1:
 			raise Exception('must have exactrly one output: distance = connection function')
 		self.iteration =0
+		
 		#LOAD Mahalanobis matrix
 		self.M = np.identity((bottom[0].channels), dtype=np.float32)
+		
+		#Parameter reading
 		param = eval(self.param_str)
        		self.changing_distance_it = param["changing_distance_it"]
           
@@ -33,17 +36,19 @@ class ConnectionFunction(caffe.Layer):
         	#connection function = output 
 		top[0].reshape(bottom[0].num)
 
+		
 	def forward(self, bottom, top):
 		self.iteration=self.iteration + 1
 		self.diff = bottom[0].data - bottom[1].data
-        	
-		if(self.iteration < self.changing_distance_it):
-			self.dist[..., 0] = np.sqrt(np.sum(self.diff**2, axis=1))   #eucl_dist	
+		
+		if(self.iteration < self.changing_distance_it):	
+        		#Euclidean distance computation
+			self.dist[..., 0] = np.sqrt(np.sum(self.diff**2, axis=1))   
 			top[0].data[...]=self.dist[..., 0]
 	
-		else:
-			self.M = bottom[2].data[...]
-			#print self.M
+		else:		
+			#Mahalanobis distance computation						
+			self.M = bottom[2].data[...]					#read mahalanobis matrix
 			for i in range(bottom[0].num):
                        		A=np.zeros(bottom[0].channels, dtype=np.float32)
 				for j in range (bottom[0].channels):
@@ -51,7 +56,6 @@ class ConnectionFunction(caffe.Layer):
                         	top[0].data[i]=sum(A*self.diff[i,:])
 
 			
-
 	def backward(self, bottom, propagate_down, top):
 		pass
 
